ee7a1e58de030dc4e8cf17d229505390c70c9773
Julio Macedo <juliocesar1076@gmail.com>
2023-03-02 10:52:38 -0300

    feat: add input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Svg, { Image } from "react-native-svg";
 import Logo from "../../../assets/Logo.svg";
+import { Input } from "../Input";
 import * as S from "./styles";
 
 export const Header = () => {
   return (
     <S.Container>
       <Logo width={110} height={32} />
+      <Input />
     </S.Container>
   );
 };
