aab477c3a874f709435064d54eb26f64f8f5932b
Julio Macedo <juliocesar1076@gmail.com>
2023-03-02 09:55:28 -0300

    feat: add header and status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Header } from "./src/components/Header";
 
 import { Home } from "./src/screens/Home";
 import { ThemeProvider } from "./src/styles/themes/Theme";
@@ -7,6 +8,8 @@ export default function App() {
   return (
     <>
       <ThemeProvider>
+        <StatusBar style="light" />
+        <Header />
         <Home />
       </ThemeProvider>
     </>
