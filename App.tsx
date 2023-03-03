0e1147b1236059507c15ed6716f6f26ab2f2036b
Julio Macedo <juliocesar1076@gmail.com>
2023-03-03 20:57:04 -0300

    feat: add context

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useContext } from "react";
 import { Header } from "./src/components/Header";
+import { TaskContextProvider } from "./src/context/tasksContext";
 
 import { Home } from "./src/screens/Home";
 import { ThemeProvider } from "./src/styles/themes/Theme";
 export default function App() {
   return (
     <>
-      <ThemeProvider>
-        <StatusBar style="light" />
-        <Header />
-        <Home />
-      </ThemeProvider>
+      <TaskContextProvider>
+        <ThemeProvider>
+          <StatusBar style="light" />
+          <Header />
+          <Home />
+        </ThemeProvider>
+      </TaskContextProvider>
     </>
   );
 }
