2fec5bf90619e54d9091410ac01c43d9dd13c729
Julio Macedo <juliocesar1076@gmail.com>
2023-03-03 23:22:20 -0300

    feat: add function in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+
 import * as S from "./styles";
 import { PlusCircle } from "phosphor-react-native";
+import { useTask } from "../../context/tasksContext";
 export const Input = () => {
+  const { handleOnChangeInput, addTask, task } = useTask();
   return (
     <S.Container>
       <S.Input
+        value={task}
+        onChangeText={(text) => handleOnChangeInput(text)}
         placeholder="Adicione uma nova tarefa"
         placeholderTextColor="#808080"
       />
-      <S.Button>
+      <S.Button onPress={() => addTask(task)}>
         <PlusCircle size={20} color="#F2F2F2" weight="bold" />
       </S.Button>
     </S.Container>
