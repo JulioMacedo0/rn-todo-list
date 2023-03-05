d6a176be0ddc010d4896f28df599caaa272937c4
Julio Macedo <juliocesar1076@gmail.com>
2023-03-05 00:41:07 -0300

    feat: use theme

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,22 +3,25 @@ import React, { useState } from "react";
 import * as S from "./styles";
 import { Trash } from "phosphor-react-native";
 import { useTask } from "../../context/tasksContext";
-import { Pressable } from "react-native";
+import { useTheme } from "styled-components/native";
 interface CardProps {
   text: string;
   id: string;
   isDone: boolean;
 }
-export const Card = ({ text, id, isDone }: CardProps) => {
+export const Card = ({ text, id, isDone, ...props }: CardProps) => {
   const { toggleTask, removeTask } = useTask();
 
+  const theme = useTheme();
+
+  console.log(props);
   return (
     <S.Container>
       <S.Pressable onPress={() => toggleTask(id)}>
         <S.CheckBox
           value={isDone}
           onValueChange={() => toggleTask(id)}
-          color={isDone ? "#5E60CE" : undefined}
+          color={isDone ? `${theme.pupleDark}` : undefined}
         />
         <S.Text isCheck={isDone}>{text}</S.Text>
       </S.Pressable>
